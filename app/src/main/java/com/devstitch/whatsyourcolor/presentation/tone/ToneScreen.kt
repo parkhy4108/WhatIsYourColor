80f33fe1ae5d682427a196f0a1fbba2a870c8c1e
parkhy4108 <parkhy4108@gmail.com>
2023-08-08 12:46:23 +0900

    Style : Update UI

--- app/src/main/java/com/devstitch/whatsyourcolor/presentation/tone/ToneScreen.kt
+++ app/src/main/java/com/devstitch/whatsyourcolor/presentation/tone/ToneScreen.kt
@@ -1,9 +1,6 @@
 package com.devstitch.whatsyourcolor.presentation.tone
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,21 +8,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.devstitch.whatsyourcolor.R
 import com.devstitch.whatsyourcolor.common.navigation.Screen
 import com.devstitch.whatsyourcolor.presentation.composable.StandardIconButton
 import com.devstitch.whatsyourcolor.presentation.composable.StandardText
+import com.devstitch.whatsyourcolor.presentation.composable.ToneColorItem
 
 @Composable
 fun ToneScreen(
@@ -41,7 +35,7 @@ fun ToneScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(20.dp,5.dp,20.dp, 20.dp),
+            .padding(20.dp, 5.dp, 20.dp, 20.dp),
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         Row(
@@ -61,77 +55,14 @@ fun ToneScreen(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
-                state.color1?.let {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                            .background(state.color1!!)
-                            .clickable {
-                                openScreen(
-                                    Screen.ColorScreen.passSeasonAndTone(
-                                        season,
-                                        state.text1
-                                    )
-                                )
-                            }
-                    ) {
-                        Text(
-                            text = state.text1,
-                            modifier = Modifier.align(Alignment.Center),
-                            fontFamily = FontFamily.Cursive,
-                            fontWeight = FontWeight.ExtraBold,
-                            fontSize = 25.sp,
-                        )
-                    }
+                ToneColorItem(modifier = Modifier.weight(1f), text = state.text1, color = state.color1) {
+                    openScreen(Screen.ColorScreen.passSeasonAndTone(season, state.text1))
                 }
-                state.color2?.let {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                            .background(state.color2!!)
-                            .clickable {
-                                openScreen(
-                                    Screen.ColorScreen.passSeasonAndTone(
-                                        season,
-                                        state.text2
-                                    )
-                                )
-                            }
-                    ) {
-                        Text(
-                            text = state.text2,
-                            modifier = Modifier.align(Alignment.Center),
-                            fontFamily = FontFamily.Cursive,
-                            fontWeight = FontWeight.ExtraBold,
-                            fontSize = 25.sp
-                        )
-                    }
+                ToneColorItem(modifier = Modifier.weight(1f),text = state.text2, color = state.color2) {
+                    openScreen(Screen.ColorScreen.passSeasonAndTone(season, state.text2))
                 }
-                state.color3?.let {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                            .background(state.color3!!)
-                            .clickable {
-                                openScreen(
-                                    Screen.ColorScreen.passSeasonAndTone(
-                                        season,
-                                        state.text3
-                                    )
-                                )
-                            }
-                    ) {
-                        Text(
-                            text = state.text3,
-                            modifier = Modifier.align(Alignment.Center),
-                            fontFamily = FontFamily.Cursive,
-                            fontWeight = FontWeight.ExtraBold,
-                            fontSize = 25.sp
-                        )
-                    }
+                ToneColorItem(modifier = Modifier.weight(1f),text = state.text3, color = state.color3) {
+                    openScreen(Screen.ColorScreen.passSeasonAndTone(season, state.text3))
                 }
             }
         }
